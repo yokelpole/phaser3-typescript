e2b75e3bb279138a398cc49f9368fd0b885f9482
Kyle Poole <poolekyle@gmail.com>
2018-11-02 21:59:39 -0700

    Websockets for mutliple players, several skins

--- src/boilerplate/objects/player.ts
+++ src/boilerplate/objects/player.ts
@@ -7,17 +7,19 @@ interface ConstructorParams {
   key: string;
   id?: string;
   isPlayer: boolean;
+  type: string;
 }
 
-export class Fighter extends Phaser.Physics.Arcade.Sprite {
+export class Player extends Phaser.Physics.Arcade.Sprite {
   public id: string = uuidv4();
   public sword: Phaser.Physics.Arcade.Sprite;
   public hasMoved: boolean = false;
+  public type: string;
 
   private cursorKeys: CursorKeys;
   private sKey: Phaser.Input.Keyboard.Key;
 
-  constructor({ scene, x, y, key, id, isPlayer }: ConstructorParams) {
+  constructor({ scene, x, y, key, id, isPlayer, type }: ConstructorParams) {
     super(scene, x, y, key);
 
     if (isPlayer) {
@@ -25,6 +27,9 @@ export class Fighter extends Phaser.Physics.Arcade.Sprite {
       this.sKey = scene.input.keyboard.addKey("S");
     }
 
+    // TODO: Might be able to not have to pass `type` as a param.
+    this.type = type;
+    this.anims.play(`${this.type}-down`);
     this.setDepth(5);
     this.id = id || uuidv4();
 
@@ -45,34 +50,34 @@ export class Fighter extends Phaser.Physics.Arcade.Sprite {
     if (this.cursorKeys.left.isDown) {
       this.x -= 4;
       if (this.sword) this.sword.x -= 4;
-      this.anims.play("left", true);
+      this.anims.play(`${this.type}-left`, true);
     } else if (this.cursorKeys.right.isDown) {
       this.x += 4;
       if (this.sword) this.sword.x += 4;
-      this.anims.play("right", true);
+      this.anims.play(`${this.type}-right`, true);
     } else if (this.cursorKeys.up.isDown) {
       this.y -= 4;
       if (this.sword) this.sword.y -= 4;
-      this.anims.play("up", true);
+      this.anims.play(`${this.type}-up`, true);
     } else if (this.cursorKeys.down.isDown) {
       this.y += 4;
       if (this.sword) this.sword.y += 4;
-      this.anims.play("down", true);
+      this.anims.play(`${this.type}-down`, true);
     }
 
     if (this.sKey.isDown && !this.sword) {
       this.sword = this.scene.physics.add.sprite(this.x, this.y, "sword");
 
-      if (this.anims.getCurrentKey() === "down") {
+      if (this.anims.getCurrentKey() === `${this.type}-down`) {
         this.sword.y += 16;
         this.sword.toggleFlipY();
         this.sword.setDepth(6); // Sword needs to be on top of fighter.
-      } else if (this.anims.getCurrentKey() === "up") {
+      } else if (this.anims.getCurrentKey() === `${this.type}-up`) {
         this.sword.y -= 16;
         this.sword.toggleFlipX();
-      } else if (this.anims.getCurrentKey() === "left") {
+      } else if (this.anims.getCurrentKey() === `${this.type}-left`) {
         this.sword.x -= 16;
-      } else if (this.anims.getCurrentKey() === "right") {
+      } else if (this.anims.getCurrentKey() === `${this.type}-right`) {
         this.sword.x += 16;
         this.sword.toggleFlipY();
         this.sword.toggleFlipX();
--- src/boilerplate/scenes/mainScene.ts
+++ src/boilerplate/scenes/mainScene.ts
@@ -4,13 +4,21 @@
  * @license      Digitsensitive
  */
 
-import { Fighter } from "../objects/fighter";
+import { Player } from "../objects/player";
 import * as _ from "lodash";
 
 export class MainScene extends Phaser.Scene {
-  private player: Fighter;
-  private otherPlayers: Fighter[] = [];
+  private player: Player;
+  private otherPlayers: Player[] = [];
   private webSocket: WebSocket;
+  private playerTypes: string[] = [
+    "fighter",
+    "thief",
+    "black-belt",
+    "red-mage",
+    "white-mage",
+    "black-mage"
+  ];
 
   constructor() {
     super({
@@ -30,76 +38,110 @@ export class MainScene extends Phaser.Scene {
     );
   }
 
-  create(): void {
-    this.anims.create({
-      key: "down",
-      frames: this.anims.generateFrameNumbers("characters", {
-        start: 0,
-        end: 1
-      }),
-      frameRate: 10
+  generateAnimationFrames() {
+    _.each(this.playerTypes, (characterType, x) => {
+      this.anims.create({
+        key: `${characterType}-down`,
+        frames: this.anims.generateFrameNumbers("characters", {
+          start: 0 + x * 27,
+          end: 1 + x * 27
+        }),
+        frameRate: 10
+      });
+      this.anims.create({
+        key: `${characterType}-up`,
+        frames: this.anims.generateFrameNumbers("characters", {
+          start: 2 + x * 27,
+          end: 3 + x * 27
+        }),
+        frameRate: 10
+      });
+      this.anims.create({
+        key: `${characterType}-right`,
+        frames: this.anims.generateFrameNumbers("characters", {
+          start: 4 + x * 27,
+          end: 5 + x * 27
+        }),
+        frameRate: 10
+      });
+      this.anims.create({
+        key: `${characterType}-left`,
+        frames: this.anims.generateFrameNumbers("characters", {
+          start: 6 + x * 27,
+          end: 7 + x * 27
+        }),
+        frameRate: 10
+      });
     });
-    this.anims.create({
-      key: "up",
-      frames: this.anims.generateFrameNumbers("characters", {
-        start: 2,
-        end: 3
-      }),
-      frameRate: 10
-    });
-    this.anims.create({
-      key: "right",
-      frames: this.anims.generateFrameNumbers("characters", {
-        start: 4,
-        end: 5
-      }),
-      frameRate: 10
-    });
-    this.anims.create({
-      key: "left",
-      frames: this.anims.generateFrameNumbers("characters", {
-        start: 6,
-        end: 7
-      }),
-      frameRate: 10
-    });
+  }
 
-    this.player = new Fighter({
+  createNewRandomPlayer() {
+    return new Player({
       scene: this,
       x: _.random(_.toNumber(this.sys.game.config.width)),
       y: _.random(_.toNumber(this.sys.game.config.height)),
       key: "characters",
+      type: _.sample(this.playerTypes),
       isPlayer: true
     });
+  }
+
+  create(): void {
+    this.generateAnimationFrames();
+
+    this.player = this.createNewRandomPlayer();
 
     this.webSocket = new WebSocket("ws://localhost:8090/ws");
     this.webSocket.onopen = () => this.sendServerPlayerLocation();
 
     this.webSocket.onmessage = message => {
-      // TODO: Make this into a try/catch
-      const resp = JSON.parse(message.data);
+      // TODO: This needs a BIG refactor.
+      // How dead players are handled is ridiculous.
+      let resp;
+      console.log(message.data);
+      try {
+        resp = JSON.parse(message.data);
+      } catch(err) {
+        throw err;
+      }
+
+      if (!resp) return;
 
       _.each(resp.objects, obj => {
-        if (obj.id === this.player.id) return;
+        if (obj.id === this.player.id) {
+          if (obj.type !== 'dead') return;
+
+          this.player.destroy();
+          this.player = this.createNewRandomPlayer();
+          this.sendServerPlayerLocation();
+          return;
+        };
 
         if (_.includes(_.map(this.otherPlayers, "id"), obj.id)) {
           const player = this.otherPlayers.find(player => player.id === obj.id);
           if (!player) return;
+          if (obj.type === 'dead') {
+            player.destroy();
+            return;
+          }
 
-          if (player.x < obj.x) player.anims.play("right", true);
-          else if (player.x > obj.x) player.anims.play("left", true);
-          else if (player.y < obj.y) player.anims.play("down", true);
-          else if (player.y > obj.y) player.anims.play("up", true);
+          if (player.x < obj.x) player.anims.play(`${player.type}-right`, true);
+          else if (player.x > obj.x) player.anims.play(`${player.type}-left`, true);
+          else if (player.y < obj.y) player.anims.play(`${player.type}-down`, true);
+          else if (player.y > obj.y) player.anims.play(`${player.type}-up`, true);
 
           player.x = obj.x;
           player.y = obj.y;
         } else {
-          const newPlayer = new Fighter({
+          if (obj.type === "dead") return;
+
+          const newPlayer = new Player({
             scene: this,
             id: obj.id,
             x: obj.x,
             y: obj.y,
             key: "characters",
+            type: obj.type,
             isPlayer: false
           });
           this.otherPlayers.push(newPlayer);
@@ -114,17 +156,17 @@ export class MainScene extends Phaser.Scene {
   }
 
   handlePlayerCollision(first: Phaser.Physics.Arcade.Sprite): void {
-    if (first.anims.getCurrentKey() === "down") first.y -= 4;
-    if (first.anims.getCurrentKey() === "up") first.y += 4;
-    if (first.anims.getCurrentKey() === "left") first.x += 4;
-    if (first.anims.getCurrentKey() === "right") first.x -= 4;
+    if (first.anims.getCurrentKey() === `${first.type}-down`) first.y -= 4;
+    if (first.anims.getCurrentKey() === `${first.type}-up`) first.y += 4;
+    if (first.anims.getCurrentKey() === `${first.type}-left`) first.x += 4;
+    if (first.anims.getCurrentKey() === `${first.type}-right`) first.x -= 4;
   }
 
   sendServerPlayerLocation() {
     this.webSocket.send(
       JSON.stringify({
         id: this.player.id,
-        type: "player",
+        type: this.player.type, 
         x: this.player.x,
         y: this.player.y
       })
@@ -144,34 +186,23 @@ export class MainScene extends Phaser.Scene {
       this.player.hasMoved = false;
     }
 
-    /*if (this.player.sword) {
+    if (this.player.sword) {
       this.physics.add.collider(
         this.player.sword,
-        this.otherDude,
+        this.otherPlayers,
         (
           sword: Phaser.Physics.Arcade.Sprite,
-          otherDude: Phaser.Physics.Arcade.Sprite
+          deadPlayer: Player
         ) => {
-          otherDude.destroy();
-          this.otherDude = null;
+          deadPlayer.destroy();
+          this.webSocket.send(
+            JSON.stringify({
+              id: deadPlayer.id,
+              type: 'dead',
+            })
+          )
         }
       );
     }
-
-    if (!this.otherDude) {
-      this.otherDude = new Fighter({
-        scene: this,
-        x: _.random(320, true),
-        y: _.random(320, true),
-        key: "characters",
-        isPlayer: false
-      });
-
-      this.physics.add.collider(
-        this.player,
-        this.otherDude,
-        this.handlePlayerCollision
-      );
-    }*/
   }
 }
